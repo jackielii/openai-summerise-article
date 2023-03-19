ce497cbbd4f98834077cb0d8ff8df549acdb64df
Jackie Li <jackie.space@gmail.com>
2023-03-19 09:45:31 +0000

    update logo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react'
+import clsx from 'clsx'
 
 import styles from './index.module.css'
 
@@ -7,6 +8,7 @@ export default function Home() {
   const [articleInput, setArticleInput] = useState('')
   const [result, setResult] = useState()
   const [loading, setLoading] = useState(false)
+  const [showResult, setShowResult] = useState(false)
 
   async function onSubmit(event) {
     setLoading(true)
@@ -30,6 +32,7 @@ export default function Home() {
       }
 
       setResult(data.result)
+      setShowResult(true)
     } catch (error) {
       setLoading(false)
       // Consider implementing your own error handling logic here
@@ -42,13 +45,13 @@ export default function Home() {
     <div>
       <Head>
         <title>Generate Summary</title>
-        <link rel="icon" href="/centrum-256x256.png" />
+        <link rel="icon" href="/target-logo.svg" />
       </Head>
 
       <main className={styles.main}>
         <nav>
           <header>
-            <img src="/centrum-256x256.png" className={styles.icon} />
+            <img src="/target-logo.svg" className={styles.icon} />
           </header>
           <span>
             <h1>Generate Summary</h1>
@@ -57,17 +60,20 @@ export default function Home() {
         <section className={styles.content}>
           <form onSubmit={onSubmit}>
             <textarea
+              className={styles.input}
               type="text"
               name="article"
               placeholder="Paste your article here"
               value={articleInput}
               onChange={(e) => setArticleInput(e.target.value)}
+              onBlur={() => setShowResult(true)}
+              onFocus={() => setShowResult(false)}
             />
             <input type="submit" disabled={loading} value="Generate" />
           </form>
           <textarea
             disabled
-            className={styles.result}
+            className={clsx(styles.result, showResult && styles.show)}
             placeholder="Summary will appear here..."
             value={loading ? 'Loading...' : result}
           />
